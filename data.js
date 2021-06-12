c04a02e4b6ae95ce50d2860caa397ea3bd35815f
kunalb04 <43133352+kunalb04@users.noreply.github.com>
2021-06-12 17:11:05 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,14 @@
+const Userdata={
+    absentDays:'9',
+    absentdetails:['1/6/2021',
+'2/6/2021',
+'3/6/2021',
+'4/6/2021'],
+leaveBalance:['CL/Contingency Leave - 6 - 31/12/2021',
+'Optional Holiday - 3 - 31/12/2021',
+'Special Privilege Leave - 10 - 31/12/2021'],
+holidaycalendar:['15th August / Sunday - Independence Day',
+'10th September / Friday - Ganesh Chaturthi',
+'2nd October / Saturday - Gandhi Jayanti']
+}
+module.exports=Userdata
